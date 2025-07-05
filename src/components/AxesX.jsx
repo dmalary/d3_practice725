0787702cee5b55fde3f41410c2894f386c858e00
madhat5 <davidmalary@gmail.com>
2025-07-05 17:00:13 -0400

    style: update x axis

--- src/components/AxesX.jsx
+++ src/components/AxesX.jsx
@@ -28,16 +28,20 @@ export default function AxesX({width, height, data}) {
           <text 
             y={-10} 
             textAnchor="middle" 
-            fontSize={10} fill="#666">
+            fontSize={16 * .8} 
+            fill="#666"
+          >
             {d3.utcFormat('%H:%M')(value)}
           </text>
           <line 
             y2={height - 20} // fix this
-            stroke="#ccc" />
+            stroke="#dddddd" />
           <text 
             y={475} 
             textAnchor="middle" 
-            fontSize={10} fill="#666">
+            fontSize={16 * .8} 
+            fill="#666"
+          >
             {d3.utcFormat('%H:%M')(value)}
           </text>
         </g>
